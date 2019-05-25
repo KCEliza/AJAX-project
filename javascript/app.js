cb9fac4aeb56ad1d9990e22581c3f75af8e2ba4e
KCEliza <48337639+KCEliza@users.noreply.github.com>
2019-05-25 12:53:45 -0600

    for loop created for making 10 gifs come up per topic

--- javascript/app.js
+++ javascript/app.js
@@ -6,5 +6,30 @@ $(document).ready(function() {
     var topic = $(this).attr("data-topic");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
     topic + "&api_key=ySd06897o3P1SJ9vxaLu3SP2edELyKXx";
-  }
+  };
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+  .then(function(response){
+    var results = response.data;
+
+for(var i = 0; i < 10; i++){
+    var gifsDiv = $("<div>");
+    var rate = results[i].rate;
+    var para = $("<p>").text("Rating: " + rate);
+    var image = $("<img>");
+    image.attr("src")
+    image.addClass("gif");
+    image.attr("data-state", "data-still");
+    image.attr("data-animate", results[i].images.fixed_height.url);
+    image.attr("data-still", results[i].images.fixed_height_still.url);
+    image.attr("src", results[i].images.fixed_height_still.url);
+    gisfDiv.prepend(para);
+      gifsDiv.prepend(image);
+      $("#giphy-view").prepend(gifsDiv);
+}
+
+});
 });
